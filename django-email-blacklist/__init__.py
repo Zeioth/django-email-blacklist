c9cb6f1b43fbedb49a164d833d469f41951576f8
zeioth <zeioth@hotmail.com>
2015-01-26 01:56:04 +0100

    V1.0 - Moving to Python 3

---
+++ django-email-blacklist/__init__.py
@@ -0,0 +1,23 @@
+from django.conf import settings
+import re
+
+
+class DisposableEmailChecker():
+    """
+    Check if an email is from a disposable
+    email service
+    """
+    
+    def __init__(self):
+        self.emails = [line.strip() for line in open(settings.DISPOSABLE_EMAIL_DOMAINS)]
+    
+    def chunk(self, l, n):
+        return (l[i:i + n] for i in range(0, len(l), n))
+
+    def is_disposable(self, email):
+        for email_group in self.chunk(self.emails, 20):
+            regex = "(.*" + ")|(.*".join(email_group) + ")"
+            if re.match(regex, email):
+                return True
+
+        return False
---
+++ setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+
+setup(
+    name="django-email-blacklist",
+    packages=['django-email-blacklist'],
+    version="1.0.0",
+    license='MIT',
+    author='Zeioth',
+    author_email='test@gmail.com',
+    description='Python class for use with Django to detect Disposable Emails.',
+    keywords="django email blacklist disposable validation",
+    install_requires='',
+    include_package_data=True,
+    url='https://github.com/Zeioth/django-email-blacklist',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3 :: Only',
+    ]
+)
